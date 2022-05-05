4d9be12c22a97c9f5c4e8851b0865e2fbc72af44
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-05-04 23:55:00 -0400

    add teachers query

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,25 @@
+const { Pool } = require ('pg');
+
+const pool = new Pool({
+  user: 'ziggy',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohort = process.argv[2];
+pool.query(`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${cohort || 'JUL02'}'
+GROUP BY teachers.name, cohort;
+  `)
+  .then(res => {
+    console.table(res.rows)
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of${user.id} and was in the ${user.cohort} cohort.`);
+    })
+  })
+  .catch(err => console.log('query error', err.stack));
